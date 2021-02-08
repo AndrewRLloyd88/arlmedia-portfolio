b4e26fb3a57c2df66deaa8bb4700d7c76f667cba
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-02-07 21:01:29 -0800

    add babel-loader to transpile jsx to js

--- themes/arlmediaTheme/assets/js/index.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-axios.get('https://api.kanye.rest').then((res) => {
-  console.log(res.data);
-});
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,40 @@
 const path = require('path');
 
 module.exports = {
-  entry: './themes/arlmediaTheme/assets/js/index.js',
+  entry: './themes/arlmediaTheme/assets/js/app.jsx',
   output: {
     filename: 'app.js',
     path: path.resolve(__dirname, 'themes', 'arlmediaTheme', 'assets', 'js'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+        include: /flexboxgrid/,
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
   },
 };
